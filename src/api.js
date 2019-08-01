54209b72df33a96abc96380675f44a7dea3acb2c
Marco A <mabo.marcoantonio@gmail.com>
2019-08-01 20:15:22 -0300

    Add comments on my code

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axio from 'axios';
 
+//Realizando uma chamada a API do Github via axios
 const api = axio.create({
   baseURL: 'https://api.github.com',
 });
--- src/main.js
+++ src/main.js
@@ -1,39 +1,49 @@
-import api from './api';
+import api from './api'; //Importando o meu script de API
 
 class App {
   constructor() {
+    // Array de repositorios
     this.repositories = [];
 
+    // Pegando os elementos
     this.formEl = document.getElementById('repo-form');
     this.inputEl = document.querySelector('input[name=repository]');
     this.listEl = document.getElementById('repo-list');
 
+    //Chamando a funcao para escutar os eventos
     this.registerHandlers();
   }
 
+  //Funcao que escuta/captura os eventos
   registerHandlers() {
+    //Quando houver um evento de submit a funcao chama outra funcao para adicionar o repositorio
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  async addRepository(event) {
-    event.preventDefault();
+  //Funcao assicrona para adicionar o repositorio na lista
+  async addRepository(event) { //Captura o evento pelo parametro
+    event.preventDefault();//Para o evento padrao
 
-    const repoInput = this.inputEl.value;
-
+    const repoInput = this.inputEl.value;//Pega o valor do input
+    
+    //Realiza uma validacao
     if (repoInput.length === 0) {
       return;
     }
 
+    //Chama funcao de Loading... Apenas para dar um parecer ao usuario
     this.setLoading();
 
+    //Bloco try_catch
     try {
-      const response = await api.get(`/repos/${repoInput}`);
+      //armazena o response da chamada da API, passando como parametro o repositorio para a busca
+      const response = await api.get(`/repos/${repoInput}`);//Utilizando a tecnica de Async/Await e de Template String
 
+      //Armazenando o conteudo que quero extrair do response da API
+      const { name, description, html_url, owner: { avatar_url } } = response.data;//Utilizando uma tecnica de destruturacao de objeto
 
-      const { name, description, html_url, owner: { avatar_url } } = response.data;
-
-
-      this.repositories.push({
+      //Adicionando o repositorio no meu Array de Repositorios
+      this.repositories.push({//Utilizando a tecnica de short-object, o nome dos atributos sao os mesmos dos valores
         name,
         description,
         avatar_url,
@@ -41,15 +51,14 @@ class App {
       });
 
       
-      this.render();
-    } catch (error) {
+      this.render();//Renderizando os novos dados para o usuario
+    } catch (error) {//Bloco Catch => Captura o erro, repositorio inexistente
       console.log(error);
-      alert('O Repositorio nao existe')
-      
+      alert('O Repositorio nao existe');//Mostra ao usuario que o repositorio buscado e inexistente
     }
     
-    this.inputEl.value = '';
-    this.setLoading(false);
+    this.inputEl.value = ''; // Limpa o input
+    this.setLoading(false); // Para o carregando, ou melhor tira a msg de carregando... para o usuario
 
   }
 
